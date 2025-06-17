05de7a026c2a735d4b124f7e039032db90391a2a
ryoma <c0a23040d7@edu.teu.ac.jp>
2025-06-17 14:38:58 +0900

    練習問題

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,43 +1,74 @@
+from encodings.punycode import T
 import os
+import random
 import sys
+from tkinter import RIGHT
+from turtle import screensize
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5), pg.K_RIGHT: (5, 0), pg.K_LEFT: (-5, 0)}
+
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    wid, hgt = True, True
+    if obj_rct.left < 0 or obj_rct.right > WIDTH:
+        wid = False
+    elif obj_rct.top > HEIGHT or obj_rct.bottom < 0:
+        hgt = False
+    return wid, hgt
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
+    vx = 5
+    vy = -5
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        screen.blit(bb_img, bb_rct)
 
+        bb_rct.move_ip([vx, vy])
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key, mv in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
 
+        bx, by = check_bound(bb_rct)
+        if not bx:
+            vx *= -1
+        elif not by:
+            vy *= -1
+        
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        
+        if kk_rct.colliderect(bb_rct):
+            return
 
 if __name__ == "__main__":
     pg.init()
